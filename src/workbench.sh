b06a93e38b721b97bd4cdcd882beb9bca27d0c38
Joseph Chang <31267839+electroaria7@users.noreply.github.com>
2025-07-30 14:30:13 -0500

    webhost update

--- src/workbench.sh
+++ src/workbench.sh
@@ -3,9 +3,5 @@
 # Authors: jia.zhou (jia [dot] zhou [at] anl [dot] gov)
 ########################################################################
 
-# Provide path to workbench-<version-number>.app/Contents for macOS
-<<<<<<< Updated upstream
-workbench_path="/Applications/Workbench-5.5.1.app/Contents"
-=======
-workbench_path="B:\Workbench-5.5.1.app/Contents"
->>>>>>> Stashed changes
+ workbench_path="/b/Workbench-5.5.1.exe"
+
